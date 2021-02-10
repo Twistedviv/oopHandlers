78ccded6945e2b6df0e8ccf8f230969806d9b341
Twistedviv_Mac <2350566976@qq.com>
2021-02-10 14:55:55 +0800

    重新命名地址接口

--- API/updateUserReceiveAddressByAddressId.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once dirname(__FILE__)."/../Service/Impl/UserServiceImpl.php";
-
-    use app\Service\Impl\UserServiceImpl;
-
-    $addressId=$_POST['addressId'];
-    $result=(new UserServiceImpl)->deleteUserReceiveAddressByAddressId($addressId);
-    echo $result;
